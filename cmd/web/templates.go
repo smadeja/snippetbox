e269a55a6cfec896e9bf1058323acb53f271ea04
Szymon Madeja <smadeja1@gmail.com>
2022-08-24 20:41:02 +0200

    Format the timestamps

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,7 +3,16 @@ package main
 import (
 	"html/template"
 	"path/filepath"
+	"time"
 )
+
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
 
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
@@ -16,7 +25,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles("./ui/html/base.tmpl")
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
 		if err != nil {
 			return nil, err
 		}
